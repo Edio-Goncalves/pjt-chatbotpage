caaf5687fc48d56d47e0b641e2ffbd3b1b2f0a1a
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-10-10 17:12:31 -0300

    Overlay menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,51 @@
+import { useState, useEffect, useRef } from "react";
 import { FaBars } from "react-icons/fa6";
 
 export default function HeaderSection() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <div className="section-home-header">
-      <div className="menu-bg-grey"></div>
-      <div className="header-wrapper">
+      <div
+        className={`menu-bg-grey ${menuOpen ? "active" : ""}`}
+        onClick={closeMenu}
+      ></div>
+      <div className="header-wrapper" ref={menuRef}>
         <div>
           <a className="logo" href="">
             ChatBot
           </a>
 
-          <nav>
-            <a href="#chatbot">Demonstração</a>
-            <a href="#planos">Planos</a>
+          <nav className={menuOpen ? "active" : ""}>
+            <a href="#chatbot" onClick={closeMenu}>
+              Demonstração
+            </a>
+            <a href="#planos" onClick={closeMenu}>
+              Planos
+            </a>
           </nav>
         </div>
 
@@ -20,7 +53,7 @@ export default function HeaderSection() {
           Contato
         </a>
 
-        <div className="bar-open">
+        <div className="bar-open" onClick={toggleMenu}>
           <FaBars />
         </div>
       </div>
