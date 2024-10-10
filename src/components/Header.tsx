ef791fe82e5626980fbfc85d3be63e7e10470590
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-10-10 15:56:00 -0300

    Adjust menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
+import { FaBars } from "react-icons/fa6";
+
 export default function HeaderSection() {
   return (
     <div className="section-home-header">
+      <div className="menu-bg-grey"></div>
       <div className="header-wrapper">
         <div>
           <a className="logo" href="">
             ChatBot
           </a>
+
           <nav>
             <a href="#chatbot">Demonstração</a>
             <a href="#planos">Planos</a>
@@ -15,6 +19,10 @@ export default function HeaderSection() {
         <a className="button" href="">
           Contato
         </a>
+
+        <div className="bar-open">
+          <FaBars />
+        </div>
       </div>
     </div>
   );
